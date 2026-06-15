3adec5b72889fa5d45eb16931b9b68960fb5a610
extraslluesma <extraslluesma@gmail.com>
2026-06-15 22:44:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Estrategia: Cache First para assets estáticos, Network First para el resto
 // ============================================================
 
-const CACHE_NAME = 'preciocompare-v2';
+const CACHE_NAME = 'preciocompare-v3';
 
 // Assets a cachear en la instalación
 const ASSETS_TO_CACHE = [
